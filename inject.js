2b4bed1ef747aad5f7d775a418c5b806808e6204
3s3s <ivanivanovkzv@gmail.com>
2015-03-17 02:49:39 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -18,8 +18,7 @@ var _3s3sObject =
 		"twitter.com",
 		"opensharing.org",
 		"800notes.com",
-		"www.facebook.com",
-		"www.youtube.com"
+		"www.facebook.com"
 		],
 	adNetworks: [
 	//	{host: "ad.a-ads.com", code: "<iframe data-aa='47233' src='//ad.a-ads.com/47233?size=468x60' scrolling='no' style='width:468px; height:60px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"},
