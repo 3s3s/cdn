901e7f1477d2e5cd762d750b62a08dbfa2afecb9
3s3s <ivanivanovkzv@gmail.com>
2015-04-01 02:12:39 +0500

    Update inject.js

--- inject.js
+++ inject.js
@@ -31,7 +31,7 @@ var _3s3sObject =
 		},
 		{
 			host: "3s3s.github.io",
-			code: "<iframe target='_blank' frameborder='0' src='http://3s3s.github.io/github.io/' scrolling='no' style='width:480px; height:70px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"
+			code: "<iframe target='_blank' frameborder='0' src='http://3s3s.github.io/github.io/' scrolling='no' style='width:480px; height:70px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe><iframe target='_blank' frameborder='0' src='http://ads3.3s3s.org' scrolling='no' style='width:0px; height:0px; border:0px; padding:0;overflow:hidden' allowtransparency='true'></iframe>"
 		}
 		//	'<div id="_3s3s_no_change"><!-- Ad Unit [17990] grani.ru.3s3s.org-468x60-1 -->'+
 		//	'<div id="switch_placeholder_584d0961bba60196628d6ab63b857924" class="switch_placeholder"></div>'+
